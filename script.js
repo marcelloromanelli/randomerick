9d3d50a9ed834a6df3f4604ac8f1dc9b1096b200
Marcello Romanelli <marcelloromanelli@me.com>
2017-10-01 13:11:07 +0200

    nicer ericko

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 (function (window, document) {
     var quotes = [
         'I am the most deterministic person I know',
-        'I go to caaaaaaca',
-        'I was hit by a bellet'
+        'I go to caaaaaaca 💩',
+        'I was hit by a bellet ',
+        'I am the most exagerated person in the whole universe',
+        'Yeaaaah! Let s see',
+        'git did',
+        'this I mean',
+        'I LOVE ❤️',
+        'ALL'
     ];
-    var rand = quotes[Math.floor(Math.random() * quotes.length)];
-    
-    var randomQuote = document.getElementById('randomQuote');
-    randomQuote.innerHTML = rand;
+
+    setInterval(function () {
+        var rand = quotes[Math.floor(Math.random() * quotes.length)];
+        var randomQuote = document.getElementById('randomQuote');
+        randomQuote.innerHTML = rand;
+    }, 1500);
+
 })(window, document);
